237bc5cd022c6f63b91778047d459684867ce0e2
chapp <chapp.hkg@gmail.com>
2023-12-01 15:49:36 +0800

    ci: add ktlint gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,21 @@
 plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.org.jlleitschuh.gradle.ktlint)
 }
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    repositories {
+        mavenCentral()
+    }
+
+    // Optionally configure plugin
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        version.set("1.0.1")
+        debug.set(true)
+    }
+}
+
 true // Needed to make the Suppress annotation work for the plugins block
