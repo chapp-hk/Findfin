0de1a3d5102b1e756727c2154ff49b42fd922af4
alan.wong <chapp.hkg@gmail.com>
2023-12-03 01:16:12 +0800

    refactor: apply ktlint custom plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,46 +3,7 @@
 plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.org.jlleitschuh.gradle.ktlint)
-}
-
-// TODO: move ktlin config and related tasks to convention plugin
-subprojects {
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
-
-    repositories {
-        mavenCentral()
-    }
-
-    // Optionally configure plugin
-    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
-        version.set("1.0.1")
-        debug.set(true)
-    }
-}
-
-tasks.register("copyGitHooks", Copy::class) {
-    description = "Copies the git hooks from /git-hooks to the .git folder."
-    group = "git hooks"
-    from("$rootDir/scripts/pre-commit")
-    into("$rootDir/.git/hooks")
-    filter { line -> line.replace("{javaHome}", System.getProperty("java.home")) }
-}
-
-tasks.register("installGitHooks", Exec::class.java) {
-    description = "Installs the pre-commit git hooks from /git-hooks."
-    group = "git hooks"
-    workingDir = rootDir
-    commandLine = listOf("chmod")
-    args("-R", "+x", ".git/hooks/")
-    dependsOn("copyGitHooks")
-    doLast {
-        logger.info("Git hook installed successfully.")
-    }
-}
-
-afterEvaluate {
-    tasks.getByPath(":app:preBuild").dependsOn(":installGitHooks")
+    id("app-ktlint-plugin")
 }
 
 true // Needed to make the Suppress annotation work for the plugins block
