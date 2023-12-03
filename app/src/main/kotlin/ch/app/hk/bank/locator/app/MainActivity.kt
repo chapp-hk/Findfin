520752320feed565659aacd9cc55fd78e579ceb6
chapp <chapp.hkg@gmail.com>
2023-12-03 23:00:51 +0800

    feat(app): apply splashscreen to main activity

--- app/src/main/kotlin/ch/app/hk/bank/locator/app/MainActivity.kt
+++ app/src/main/kotlin/ch/app/hk/bank/locator/app/MainActivity.kt
@@ -1,6 +1,12 @@
 package ch.app.hk.bank.locator.app
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+    }
 }
