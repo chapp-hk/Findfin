ec1402810f7c1e94acca42d278c2387e7741bf0a
chapp <chapp.hkg@gmail.com>
2023-12-06 23:46:05 +0800

    feat(build-logic): add android test plugin

--- build-logic/plugins/src/main/kotlin/ch/app/hk/bank/locator/buildlogic/android/AndroidCommonPlugin.kt
+++ build-logic/plugins/src/main/kotlin/ch/app/hk/bank/locator/buildlogic/android/AndroidCommonPlugin.kt
@@ -1,5 +1,7 @@
 package ch.app.hk.bank.locator.buildlogic.android
 
+import ch.app.hk.bank.locator.buildlogic.testing.AndroidTestPlugin
+import com.android.build.api.dsl.CommonExtension
 import com.android.build.api.variant.AndroidComponentsExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
@@ -9,33 +11,48 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 class AndroidCommonPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        val androidComponents = project.extensions.getByType(AndroidComponentsExtension::class.java)
-
-        androidComponents.finalizeDsl { extension ->
-            extension.compileSdk = 34
-            extension.buildToolsVersion = "34.0.0"
-
-            extension.defaultConfig {
-                minSdk = 24
-                testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        @Suppress("UnstableApiUsage")
+        project
+            .extensions
+            .getByType(AndroidComponentsExtension::class.java)
+            .finalizeDsl { extension ->
+                configureVersion(extension = extension)
+                configureBuiltTypes(extension = extension)
             }
 
-            extension.compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
-            }
+        AndroidTestPlugin().apply(project)
 
-            extension.buildTypes {
-                getByName("release") {
-                    it.isMinifyEnabled = false
-                    it.proguardFiles(
-                        extension.getDefaultProguardFile("proguard-android-optimize.txt"),
-                        "proguard-rules.pro",
-                    )
-                }
-            }
+        configKotlin(project = project)
+    }
+
+    private fun configureVersion(extension: CommonExtension<*, *, *, *, *>) {
+        extension.compileSdk = 34
+        extension.buildToolsVersion = "34.0.0"
+
+        extension.defaultConfig {
+            minSdk = 24
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         }
 
+        extension.compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+        }
+    }
+
+    private fun configureBuiltTypes(extension: CommonExtension<*, *, *, *, *>) {
+        extension.buildTypes {
+            getByName("release") {
+                it.isMinifyEnabled = false
+                it.proguardFiles(
+                    extension.getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro",
+                )
+            }
+        }
+    }
+
+    private fun configKotlin(project: Project) {
         project.tasks.withType<KotlinCompile>().configureEach {
             it.kotlinOptions {
                 jvmTarget = JavaVersion.VERSION_17.toString()
