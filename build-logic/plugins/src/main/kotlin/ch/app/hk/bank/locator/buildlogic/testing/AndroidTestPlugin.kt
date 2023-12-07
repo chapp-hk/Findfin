09b6d392c8043019744e7de24cbc5464c6c6ca31
chApp <chapp.hkg@gmail.com>
2023-12-08 00:37:25 +0800

    feat: gradle managed devices (#11)
    * feat(build-logic): add android test plugin
    
    * refactor(ci): change android-ci to reusable workflow
    
    * refactor(ci): update steps to setup gradle
    
    * refactor(ci): update workflow names
    
    * feat(ci): add instrument test workflow
    
    * feat(ci): update instrument test and add cancel policy
    
    * feat(ci): move instrument test to release workflow

---
+++ build-logic/plugins/src/main/kotlin/ch/app/hk/bank/locator/buildlogic/testing/AndroidTestPlugin.kt
@@ -0,0 +1,34 @@
+package ch.app.hk.bank.locator.buildlogic.testing
+
+import com.android.build.api.variant.AndroidComponentsExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AndroidTestPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        val androidComponents = project.extensions.getByType(AndroidComponentsExtension::class.java)
+
+        @Suppress("UnstableApiUsage")
+        androidComponents.finalizeDsl { extension ->
+            extension.buildTypes {
+                getByName("debug") {
+                    it.enableUnitTestCoverage = true
+                    it.enableAndroidTestCoverage = true
+                }
+            }
+
+            extension.testOptions {
+                managedDevices {
+                    localDevices.create("pixel2api30") {
+                        // Use device profiles you typically see in Android Studio.
+                        it.device = "Pixel 2"
+                        // Use only API levels 27 and higher.
+                        it.apiLevel = 30
+                        // To include Google services, use "google".
+                        it.systemImageSource = "aosp"
+                    }
+                }
+            }
+        }
+    }
+}
