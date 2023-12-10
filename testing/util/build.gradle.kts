36ff2b37da8a6eebc4d3d5700b8656090e9a9857
chapp <chapp.hkg@gmail.com>
2023-12-10 23:49:33 +0800

    feat(testing): add testing util module

---
+++ testing/util/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.org.jetbrains.kotlin.jvm)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.kotest.assertions.core)
+}
