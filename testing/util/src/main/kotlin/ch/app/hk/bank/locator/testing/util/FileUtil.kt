0ad1aa0f04da039eb6e3e0e38934fee0efa1fb50
chapp <chapp.hkg@gmail.com>
2023-12-11 16:19:50 +0800

    feat(testing-util): add read resource as object function

--- testing/util/src/main/kotlin/ch/app/hk/bank/locator/testing/util/FileUtil.kt
+++ testing/util/src/main/kotlin/ch/app/hk/bank/locator/testing/util/FileUtil.kt
@@ -1,5 +1,14 @@
 package ch.app.hk.bank.locator.testing.util
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 fun Any.readResourceAsText(path: String): String {
     return javaClass.classLoader.getResource(path)?.readText().orEmpty()
 }
+
+inline fun <reified T> Any.readResourceAsObject(path: String): T =
+    Gson().fromJson(
+        readResourceAsText(path),
+        object : TypeToken<T>() {}.type,
+    )
