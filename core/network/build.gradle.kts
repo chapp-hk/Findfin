b44ad6b829de74681c379f15619b8046da01ce5a
chapp <chapp.hkg@gmail.com>
2023-12-11 11:52:34 +0800

    test(core-network): add unit test

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -7,10 +7,19 @@ java {
     targetCompatibility = JavaVersion.VERSION_17
 }
 
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
 dependencies {
     implementation(libs.ktor.core)
     implementation(libs.ktor.content.negotiation)
     implementation(libs.ktor.serialization.json)
     implementation(libs.ktor.logging)
     implementation(libs.ktor.resources)
+
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.kotest.assertions.core)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.ktor.android)
 }
