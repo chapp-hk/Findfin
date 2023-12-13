2dd1cf3f8abf7eaaebac1b578ae6c1414beca4d0
chapp <chapp.hkg@gmail.com>
2023-12-13 15:11:47 +0800

    chore(app): remove test code

--- app/src/main/kotlin/ch/app/hk/bank/locator/app/MainActivity.kt
+++ app/src/main/kotlin/ch/app/hk/bank/locator/app/MainActivity.kt
@@ -5,15 +5,10 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import ch.app.hk.bank.locator.core.design.theme.AppTheme
-import ch.app.hk.bank.locator.feature.locator.data.remote.datasource.LocatorRemoteDataSource
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    @Inject
-    lateinit var LocatorRemoteDataSource: LocatorRemoteDataSource
-
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
