9204ea1a755ec765609ef573055d08b126e04617
chapp <chapp.hkg@gmail.com>
2023-12-15 16:02:50 +0800

    feat(framework-hiltext): add room annotation processor

---
+++ framework/hiltext/processor-room/src/test/kotlin/ch/app/framework/hiltext/room/HiltExtRoomModuleTest.kt
@@ -0,0 +1,99 @@
+package ch.app.framework.hiltext.room
+
+import ch.app.framework.hiltext.room.processor.HiltExtRoomProcessorProvider
+import ch.app.hk.bank.locator.testing.util.readResourceAsText
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
+import com.tschuchort.compiletesting.kspSourcesDir
+import com.tschuchort.compiletesting.symbolProcessorProviders
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.io.File
+
+@DisplayName("@HiltExtRoomModule tests")
+class HiltExtRoomModuleTest {
+    private val generatedCodePath = "/kotlin/ch/app/framework/hiltext/room/"
+    private val generatedFileNamePostfix = "HiltExtRoomModule"
+
+    @Test
+    fun `test @HiltExtRoomModule with default parameter values`() {
+        val testClassName = "TestDatabaseDefaultParameters"
+        val kotlinSource =
+            SourceFile.kotlin(
+                name = "$testClassName.kt",
+                contents = readResourceAsText("source/test-default-params.source"),
+            )
+
+        val compilation = compileKotlinSource(kotlinSource)
+        compilation.compile()
+
+        File(
+            "${compilation.kspSourcesDir.path}${generatedCodePath}${testClassName}$generatedFileNamePostfix.kt",
+        ).readText() shouldBe
+            readResourceAsText("expected/generated-default-params.source")
+    }
+
+    @Test
+    fun `test @HiltExtRoomModule with custom parameter values`() {
+        val testClassName = "TestDatabaseCustomParameters"
+        val kotlinSource =
+            SourceFile.kotlin(
+                name = "$testClassName.kt",
+                contents = readResourceAsText("source/test-custom-params.source"),
+            )
+
+        val compilation = compileKotlinSource(kotlinSource)
+        compilation.compile()
+
+        File(
+            "${compilation.kspSourcesDir.path}${generatedCodePath}${testClassName}$generatedFileNamePostfix.kt",
+        ).readText() shouldBe
+            readResourceAsText("expected/generated-custom-params.source")
+    }
+
+    @Test
+    fun `test @HiltExtRoomModule with empty databaseName`() {
+        val kotlinSource =
+            SourceFile.kotlin(
+                name = "TestEmptyDatabaseName.kt",
+                contents = readResourceAsText("source/test-empty-database-name.source"),
+            )
+
+        val compilation = compileKotlinSource(kotlinSource)
+        compilation.compile().exitCode shouldBe KotlinCompilation.ExitCode.COMPILATION_ERROR
+    }
+
+    @Test
+    fun `test @HiltExtRoomModule without @Database`() {
+        val kotlinSource =
+            SourceFile.kotlin(
+                name = "TestWithoutDatabaseAnnotation.kt",
+                contents = readResourceAsText("source/test-without-database-annotation.source"),
+            )
+
+        val compilation = compileKotlinSource(kotlinSource)
+        compilation.compile().exitCode shouldBe KotlinCompilation.ExitCode.COMPILATION_ERROR
+    }
+
+    @Test
+    fun `test @HiltExtRoomModule with custom scope not annotated with @Scope`() {
+        val kotlinSource =
+            SourceFile.kotlin(
+                name = "TestCustomScopeNotAnnotatedWithScope.kt",
+                contents = readResourceAsText("source/test-custom-scope-without-annotated.source"),
+            )
+
+        val compilation = compileKotlinSource(kotlinSource)
+        compilation.compile().exitCode shouldBe KotlinCompilation.ExitCode.COMPILATION_ERROR
+    }
+
+    private fun compileKotlinSource(source: SourceFile): KotlinCompilation {
+        return KotlinCompilation().apply {
+            inheritClassPath = true
+            workingDir = File("build/tmp/Kotlin-Compilation")
+            sources = listOf(source)
+            symbolProcessorProviders = listOf(HiltExtRoomProcessorProvider())
+        }
+    }
+}
