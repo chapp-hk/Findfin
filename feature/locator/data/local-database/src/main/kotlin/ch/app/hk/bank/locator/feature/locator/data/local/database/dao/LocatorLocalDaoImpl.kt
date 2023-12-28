17dfe48a539ea13cfcdb461059849b5cb32b5bb5
chapp <chapp.hkg@gmail.com>
2023-12-28 10:54:14 +0800

    test(locator-data-local-database): add unit test

--- feature/locator/data/local-database/src/main/kotlin/ch/app/hk/bank/locator/feature/locator/data/local/database/dao/LocatorLocalDaoImpl.kt
+++ feature/locator/data/local-database/src/main/kotlin/ch/app/hk/bank/locator/feature/locator/data/local/database/dao/LocatorLocalDaoImpl.kt
@@ -4,6 +4,7 @@ import ch.app.framework.hiltext.annotation.HiltExtBindModule
 import ch.app.hk.bank.locator.feature.locator.data.local.dao.LocatorDao
 import ch.app.hk.bank.locator.feature.locator.data.local.database.entity.BankMapper
 import ch.app.hk.bank.locator.feature.locator.data.local.entity.BankLocal
+import org.mapstruct.factory.Mappers
 import javax.inject.Inject
 
 @HiltExtBindModule
@@ -11,9 +12,9 @@ class LocatorLocalDaoImpl
     @Inject
     constructor(
         private val locatorRoomDao: LocatorRoomDao,
-        private val bankMapper: BankMapper,
     ) : LocatorDao {
         override suspend fun insertAll(banks: List<BankLocal>) {
+            val bankMapper = Mappers.getMapper(BankMapper::class.java)
             locatorRoomDao.insertAll(banks.map(bankMapper::clone))
         }
     }
---
+++ feature/locator/data/local-database/src/test/kotlin/ch/app/hk/bank/locator/feature/locator/data/local/database/dao/LocatorLocalDaoImplTest.kt
@@ -0,0 +1,43 @@
+package ch.app.hk.bank.locator.feature.locator.data.local.database.dao
+
+import ch.app.hk.bank.locator.feature.locator.data.local.database.entity.BankEntity
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.slot
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("LocatorLocalDaoImpl unit tests")
+class LocatorLocalDaoImplTest {
+    private val locatorRoomDao = mockk<LocatorRoomDao>()
+
+    private val locatorLocalDaoImpl =
+        LocatorLocalDaoImpl(
+            locatorRoomDao = locatorRoomDao,
+        )
+
+    @Test
+    @DisplayName(
+        "When invoke insertAll() with list of BankLocal, " +
+            "should convert to list of BankEntity and invoke LocatorLocalDaoImpl.insertAll()",
+    )
+    fun testInsertAll() =
+        runTest(StandardTestDispatcher()) {
+            val bankEntityListSlot = slot<List<BankEntity>>()
+
+            coEvery {
+                locatorRoomDao.insertAll(capture(bankEntityListSlot))
+            } just Runs
+
+            locatorLocalDaoImpl.insertAll(listOf(mockk(relaxed = true)))
+
+            coVerify {
+                locatorRoomDao.insertAll(bankEntityListSlot.captured)
+            }
+        }
+}
