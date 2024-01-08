afd76a9995251ccd7a717a12f1b2258f2fe14027
chapp <chapp.hkg@gmail.com>
2024-01-08 23:37:56 +0800

    test(locator-data-local-database): update bank mapper unit test

--- feature/locator/data/local-database/src/test/kotlin/ch/app/hk/bank/locator/feature/locator/data/local/database/entity/BankMapperTest.kt
+++ feature/locator/data/local-database/src/test/kotlin/ch/app/hk/bank/locator/feature/locator/data/local/database/entity/BankMapperTest.kt
@@ -13,16 +13,16 @@ class BankMapperTest {
 
     @Test
     fun `test clone`() {
-        val bank = object : BankLocal {
-            override val type: String = "some type"
-            override val district: String = "somewhere"
-            override val bankName: String = "mock bank"
-            override val typeName: String = "mock type"
-            override val address: String = "some street"
-            override val serviceHours: String = "24 hours"
-            override val latitude: Double = 22.24214
-            override val longitude: Double = 144.5325
-        }
+        val bank = BankLocal(
+            type = "some type",
+            district = "somewhere",
+            bankName = "mock bank",
+            typeName = "mock type",
+            address = "some street",
+            serviceHours = "24 hours",
+            latitude = 22.24214,
+            longitude = 144.5325,
+        )
 
         bankMapper.clone(bank) shouldBe
             BankEntity(
