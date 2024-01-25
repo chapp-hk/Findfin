2dada81b83987dfe71ac681eaaef36e48aabceb0
chapp <chapp.hkg@gmail.com>
2024-01-25 21:28:37 +0800

    refactor(locale): add locale repository interface and implementation

---
+++ core/locale/api/src/main/kotlin/ch/app/hk/bank/locator/core/locale/api/AppLocaleRepository.kt
@@ -0,0 +1,11 @@
+package ch.app.hk.bank.locator.core.locale.api
+
+import java.util.Locale
+
+interface AppLocaleRepository {
+    fun setLocale(locale: String)
+
+    fun getCurrentLocale(): Locale?
+
+    fun availableLocales(): List<AppLocale>
+}
