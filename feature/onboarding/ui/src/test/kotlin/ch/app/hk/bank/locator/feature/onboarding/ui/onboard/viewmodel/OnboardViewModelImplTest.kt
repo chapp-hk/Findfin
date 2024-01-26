708166943221213b7e502a88bee7ecdf50b68edf
chapp <chapp.hkg@gmail.com>
2024-01-26 21:21:02 +0800

    test(onboarding): change to argument source provider

--- feature/onboarding/ui/src/test/kotlin/ch/app/hk/bank/locator/feature/onboarding/ui/onboard/viewmodel/OnboardViewModelImplTest.kt
+++ feature/onboarding/ui/src/test/kotlin/ch/app/hk/bank/locator/feature/onboarding/ui/onboard/viewmodel/OnboardViewModelImplTest.kt
@@ -9,23 +9,25 @@ import io.mockk.mockk
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.Arguments.arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 @ExtendWith(MainDispatcherExtension::class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("OnboardViewModelImpl unit tests")
 class OnboardViewModelImplTest {
     private val appPreferencesRepository = mockk<AppPreferencesRepository>()
 
     @ParameterizedTest(
         name = "test uiState, when appPreferencesRepository.getBoolean() returns {0}, " +
-            "then OnboardViewModelImpl.uiState should be {1}"
+            "then OnboardViewModelImpl.uiState should be {1}",
     )
-    @MethodSource("uiStateArguments")
+    @ArgumentsSource(UiStateArgumentProvider::class)
     fun testUiState(
         mockedReturnValue: Boolean,
         expectedResult: OnboardUiState,
@@ -41,11 +43,14 @@ class OnboardViewModelImplTest {
         }
     }
 
-    private fun uiStateArguments() =
-        listOf(
-            arguments(false, OnboardUiState.SelectLanguage),
-            arguments(true, OnboardUiState.NavigateToHome),
-        )
+    private class UiStateArgumentProvider : ArgumentsProvider {
+        override fun provideArguments(p0: ExtensionContext?): Stream<out Arguments> {
+            return Stream.of(
+                arguments(false, OnboardUiState.SelectLanguage),
+                arguments(true, OnboardUiState.NavigateToHome),
+            )
+        }
+    }
 
     private fun createOnboardViewModel() =
         OnboardViewModelImpl(appPreferencesRepository = appPreferencesRepository)
