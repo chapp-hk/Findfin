609f5622aba13c7ed4356b125985f127646a8712
chapp <chapp.hkg@gmail.com>
2024-02-03 12:16:07 +0800

    feat(core-design): add error view

---
+++ core/design/src/main/kotlin/ch/app/hk/bank/locator/core/design/ui/ErrorView.kt
@@ -0,0 +1,38 @@
+package ch.app.hk.bank.locator.core.design.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ErrorView(
+    message: String,
+    retryButtonText: String,
+    retry: () -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = message,
+            style = MaterialTheme.typography.titleSmall,
+        )
+
+        Button(
+            onClick = retry,
+        ) {
+            Text(text = retryButtonText)
+        }
+    }
+}
