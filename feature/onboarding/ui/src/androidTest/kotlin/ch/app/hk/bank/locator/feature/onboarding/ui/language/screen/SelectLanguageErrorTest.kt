5fc196fbe82008593d6e6d4262fc867031baf121
chapp <chapp.hkg@gmail.com>
2024-02-04 23:31:39 +0800

    test(onboarding-ui): add onboarding select language error tests

---
+++ feature/onboarding/ui/src/androidTest/kotlin/ch/app/hk/bank/locator/feature/onboarding/ui/language/screen/SelectLanguageErrorTest.kt
@@ -0,0 +1,39 @@
+package ch.app.hk.bank.locator.feature.onboarding.ui.language.screen
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import ch.app.hk.bank.locator.core.design.theme.AppTheme
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Rule
+import org.junit.Test
+
+
+class SelectLanguageErrorTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun verifyRetryInvoke() {
+        val mockRetry = mockk<() -> Unit>()
+        every { mockRetry() } just Runs
+
+        composeTestRule.setContent {
+            AppTheme {
+                SelectLanguageError(
+                    retry = mockRetry
+                )
+            }
+        }
+
+        composeTestRule
+            .onNodeWithTag(TEST_TAG_ONBOARDING_SELECT_LANGUAGE_ERROR_RETRY)
+            .performClick()
+
+        verify { mockRetry() }
+    }
+}
