94928b77773fa0ae7c12cc23244e6edd74ada1d2
chapp <chapp.hkg@gmail.com>
2024-02-09 21:53:41 +0800

    feat(app): add home navigation graph

--- app/src/main/kotlin/ch/app/hk/bank/locator/app/navigation/AppNavHost.kt
+++ app/src/main/kotlin/ch/app/hk/bank/locator/app/navigation/AppNavHost.kt
@@ -6,6 +6,8 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import ch.app.hk.bank.locator.feature.home.navigation.HomeNavGraphDestination
+import ch.app.hk.bank.locator.feature.home.navigation.homeNavGraph
 import ch.app.hk.bank.locator.feature.onboarding.navigation.OnboardingNavGraphDestination
 import ch.app.hk.bank.locator.feature.onboarding.navigation.onboardingNavGraph
 
@@ -19,6 +21,16 @@ fun AppNavHost(
         navController = navController,
         startDestination = OnboardingNavGraphDestination.navGraphId,
     ) {
-        onboardingNavGraph(navController)
+        onboardingNavGraph(
+            navController = navController,
+            goToHome = {
+                navController.navigate(HomeNavGraphDestination.navGraphId) {
+                    popUpTo(OnboardingNavGraphDestination.navGraphId) {
+                        inclusive = true
+                    }
+                }
+            },
+        )
+        homeNavGraph(navController)
     }
 }
