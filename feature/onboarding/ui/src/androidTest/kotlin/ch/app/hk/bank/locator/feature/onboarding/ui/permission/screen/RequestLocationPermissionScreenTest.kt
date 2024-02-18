8e3abfb5c2999e52163abeefd6659648996a29cb
chapp <chapp.hkg@gmail.com>
2024-02-18 13:10:28 +0800

    feat(onboarding): add navigation to home

---
+++ feature/onboarding/ui/src/androidTest/kotlin/ch/app/hk/bank/locator/feature/onboarding/ui/permission/screen/RequestLocationPermissionScreenTest.kt
@@ -0,0 +1,62 @@
+package ch.app.hk.bank.locator.feature.onboarding.ui.permission.screen
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import ch.app.hk.bank.locator.core.design.theme.AppTheme
+import ch.app.hk.bank.locator.core.design.ui.ScreenState
+import ch.app.hk.bank.locator.feature.onboarding.ui.permission.viewmodel.PermissionViewModel
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.junit.Rule
+import org.junit.Test
+
+class RequestLocationPermissionScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private val permissionViewModel = mockk<PermissionViewModel>()
+
+    @Test
+    fun assertInitialUiIsDisplayed() {
+        every { permissionViewModel.uiState } returns
+            MutableStateFlow(ScreenState.Empty)
+
+        composeTestRule.setContent {
+            AppTheme {
+                RequestLocationPermissionScreen(
+                    permissionViewModel = permissionViewModel,
+                    goToHome = {},
+                )
+            }
+        }
+
+        composeTestRule
+            .onNodeWithTag(TEST_TAG_ONBOARDING_REQUEST_LOCATION_PERMISSION_SCREEN)
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun verifySuccess() {
+        every { permissionViewModel.uiState } returns
+            MutableStateFlow(ScreenState.Success(true))
+
+        val mockGoToHome = mockk<() -> Unit>()
+        every { mockGoToHome() } just Runs
+
+        composeTestRule.setContent {
+            AppTheme {
+                RequestLocationPermissionScreen(
+                    permissionViewModel = permissionViewModel,
+                    goToHome = mockGoToHome,
+                )
+            }
+        }
+
+        verify { mockGoToHome() }
+    }
+}
---
+++ feature/onboarding/ui/src/main/kotlin/ch/app/hk/bank/locator/feature/onboarding/ui/permission/screen/RequestLocationPermissionContent.kt
@@ -0,0 +1,148 @@
+package ch.app.hk.bank.locator.feature.onboarding.ui.permission.screen
+
+import android.Manifest
+import android.content.res.Configuration
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ch.app.hk.bank.locator.core.design.theme.AppTheme
+import ch.app.hk.bank.locator.feature.onboarding.ui.R
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+@Composable
+fun RequestLocationPermissionContent(
+    modifier: Modifier = Modifier,
+    completeOnboarding: () -> Unit,
+) {
+    val isShowDialog = remember { mutableStateOf(false) }
+    LocationPermissionNotGrantedDialog(
+        isShowDialog = isShowDialog.value,
+    ) {
+        completeOnboarding()
+    }
+
+    val launcher =
+        rememberLauncherForActivityResult(
+            ActivityResultContracts.RequestPermission(),
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                completeOnboarding()
+            } else {
+                isShowDialog.value = true
+            }
+        }
+
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+    ) {
+        Column(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(
+                        top = it.calculateTopPadding(),
+                        bottom = it.calculateBottomPadding(),
+                    )
+                    .padding(
+                        top = 55.dp,
+                        start = 16.dp,
+                        end = 16.dp,
+                        bottom = 24.dp,
+                    ),
+        ) {
+            Text(
+                modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
+                text = stringResource(id = R.string.onboarding_title_enable_location),
+                style = MaterialTheme.typography.headlineSmall,
+            )
+
+            Text(
+                modifier =
+                    Modifier
+                        .padding(top = 8.dp)
+                        .align(alignment = Alignment.CenterHorizontally),
+                text = stringResource(id = R.string.onboarding_subtitle_enable_location),
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.bodySmall,
+            )
+
+            val lottieResource =
+                LottieCompositionSpec.RawRes(R.raw.onboarding_request_location_permission)
+            val composition by rememberLottieComposition(lottieResource)
+            LottieAnimation(
+                modifier =
+                    Modifier
+                        .padding(top = 8.dp)
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .align(alignment = Alignment.CenterHorizontally),
+                composition = composition,
+                iterations = LottieConstants.IterateForever,
+            )
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    launcher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                },
+            ) {
+                Text(text = stringResource(id = R.string.onboarding_button_grant_permission))
+            }
+
+            OutlinedButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    isShowDialog.value = true
+                },
+            ) {
+                Text(text = stringResource(id = R.string.onboarding_button_skip))
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = android.graphics.Color.WHITE.toLong(),
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+)
+@Composable
+private fun RequestLocationPermissionContentPreviewDay() {
+    AppTheme {
+        RequestLocationPermissionContent {}
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = android.graphics.Color.BLACK.toLong(),
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Composable
+private fun RequestLocationPermissionContentPreviewNight() {
+    AppTheme {
+        RequestLocationPermissionContent {}
+    }
+}
