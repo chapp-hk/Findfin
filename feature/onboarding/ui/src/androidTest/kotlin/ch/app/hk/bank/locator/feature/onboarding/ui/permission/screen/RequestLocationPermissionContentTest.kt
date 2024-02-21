95cd4e350c7a8172cfd9667a54803929ea0adffd
chapp <chapp.hkg@gmail.com>
2024-02-21 16:02:06 +0800

    test(onboarding-ui): add ui test for request location permission content

---
+++ feature/onboarding/ui/src/androidTest/kotlin/ch/app/hk/bank/locator/feature/onboarding/ui/permission/screen/RequestLocationPermissionContentTest.kt
@@ -0,0 +1,60 @@
+package ch.app.hk.bank.locator.feature.onboarding.ui.permission.screen
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import ch.app.hk.bank.locator.core.design.theme.AppTheme
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Rule
+import org.junit.Test
+
+class RequestLocationPermissionContentTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun verifyOnGrantPermission() {
+        val mockOnGrantPermission = mockk<() -> Unit>()
+        every { mockOnGrantPermission() } just Runs
+
+        composeTestRule.setContent {
+            AppTheme {
+                RequestLocationPermissionContent(
+                    onGrantPermission = mockOnGrantPermission,
+                    onSkip = {},
+                )
+            }
+        }
+
+        composeTestRule
+            .onNodeWithTag(TEST_TAG_ONBOARDING_REQUEST_LOCATION_PERMISSION_BUTTON_GRANT)
+            .performClick()
+
+        verify { mockOnGrantPermission() }
+    }
+
+    @Test
+    fun verifySkip() {
+        val mockOnSkip = mockk<() -> Unit>()
+        every { mockOnSkip() } just Runs
+
+        composeTestRule.setContent {
+            AppTheme {
+                RequestLocationPermissionContent(
+                    onGrantPermission = {},
+                    onSkip = mockOnSkip,
+                )
+            }
+        }
+
+        composeTestRule
+            .onNodeWithTag(TEST_TAG_ONBOARDING_REQUEST_LOCATION_PERMISSION_BUTTON_SKIP)
+            .performClick()
+
+        verify { mockOnSkip() }
+    }
+}
