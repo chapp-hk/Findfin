7048f08ed84b3bf4016d59240d5163dd4cd8e45a
chapp <chapp.hkg@gmail.com>
2024-02-24 19:24:02 +0800

    build(build-logic): bump kotlinCompilerExtensionVersion to 1.5.10

--- build-logic/convention/src/main/kotlin/ch/app/hk/bank/locator/buildlogic/plugin/compose/ComposePlugin.kt
+++ build-logic/convention/src/main/kotlin/ch/app/hk/bank/locator/buildlogic/plugin/compose/ComposePlugin.kt
@@ -18,7 +18,7 @@ class ComposePlugin : Plugin<Project> {
             }
 
             extension.composeOptions {
-                kotlinCompilerExtensionVersion = "1.5.8"
+                kotlinCompilerExtensionVersion = "1.5.10"
             }
 
             project.dependencies {
