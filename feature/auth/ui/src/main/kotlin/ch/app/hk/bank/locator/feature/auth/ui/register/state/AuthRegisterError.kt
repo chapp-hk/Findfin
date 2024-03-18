b2ecc3dd2fe4bf5e3c2d956100bb9e45e76eaa71
chapp <chapp.hkg@gmail.com>
2024-03-18 23:59:57 +0800

    feat(auth-ui): add email password register in register viewmodel

---
+++ feature/auth/ui/src/main/kotlin/ch/app/hk/bank/locator/feature/auth/ui/register/state/AuthRegisterError.kt
@@ -0,0 +1,8 @@
+package ch.app.hk.bank.locator.feature.auth.ui.register.state
+
+enum class AuthRegisterError {
+    UNKNOWN,
+    INVALID_EMAIL,
+    WEAK_PASSWORD,
+    EMAIL_ALREADY_IN_USE,
+}
--- feature/auth/ui/src/test/kotlin/ch/app/hk/bank/locator/feature/auth/ui/register/viewmodel/AuthRegisterViewModelImplTest.kt
+++ feature/auth/ui/src/test/kotlin/ch/app/hk/bank/locator/feature/auth/ui/register/viewmodel/AuthRegisterViewModelImplTest.kt
@@ -4,6 +4,7 @@ import app.cash.turbine.test
 import ch.app.hk.bank.locator.core.design.ui.ScreenState
 import ch.app.hk.bank.locator.feature.auth.data.repo.model.AuthResult
 import ch.app.hk.bank.locator.feature.auth.data.repo.repository.AuthRepository
+import ch.app.hk.bank.locator.feature.auth.ui.register.state.AuthRegisterError
 import ch.app.hk.bank.locator.feature.auth.ui.register.state.AuthRegisterUiState
 import ch.app.hk.bank.locator.testing.extension.MainDispatcherExtension
 import io.kotest.matchers.equality.shouldBeEqualToComparingFields
@@ -12,6 +13,7 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
@@ -57,10 +59,84 @@ class AuthRegisterViewModelImplTest {
                     ScreenState.Success(AuthRegisterUiState.Authorized),
                 ),
                 Arguments.arguments(
-                    AuthResult.Failed(""),
-                    ScreenState.Error(AuthRegisterUiState.Failed),
+                    AuthResult.Error.Unknown,
+                    ScreenState.Error(AuthRegisterUiState.Error(AuthRegisterError.UNKNOWN)),
                 ),
             )
         }
     }
+
+    @ParameterizedTest(
+        name =
+            "when authRepository.emailPasswordRegister() returns {0}, " +
+                "then AuthRegisterViewModelImpl.uiState should be {1}",
+    )
+    @ArgumentsSource(EmailPasswordRegisterArgumentProvider::class)
+    fun `test emailPasswordRegister`(
+        mockAuthRepositoryAnonymousLoginValue: AuthResult,
+        expectedResult: ScreenState<AuthRegisterUiState>,
+    ) = runTest {
+        coEvery {
+            authRepository.emailPasswordRegister(
+                email = any(),
+                password = any(),
+            )
+        } returns mockAuthRepositoryAnonymousLoginValue
+
+        authRegisterViewModel.emailPasswordRegister(
+            email = "name@test.com",
+            password = "123456",
+        )
+
+        authRegisterViewModel.uiState.test {
+            awaitItem() shouldBe ScreenState.Empty
+            awaitItem() shouldBe ScreenState.Loading
+            awaitItem() shouldBeEqualToComparingFields expectedResult
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    private class EmailPasswordRegisterArgumentProvider : ArgumentsProvider {
+        override fun provideArguments(extensionContext: ExtensionContext): Stream<Arguments> {
+            return Stream.of(
+                Arguments.arguments(
+                    AuthResult.Authorized,
+                    ScreenState.Success(AuthRegisterUiState.Authorized),
+                ),
+                Arguments.arguments(
+                    AuthResult.Error.Unknown,
+                    ScreenState.Error(AuthRegisterUiState.Error(AuthRegisterError.UNKNOWN)),
+                ),
+                Arguments.arguments(
+                    AuthResult.Error.Register.EmailAlreadyInUse,
+                    ScreenState.Error(AuthRegisterUiState.Error(AuthRegisterError.EMAIL_ALREADY_IN_USE)),
+                ),
+                Arguments.arguments(
+                    AuthResult.Error.Register.WeakPassword,
+                    ScreenState.Error(AuthRegisterUiState.Error(AuthRegisterError.WEAK_PASSWORD)),
+                ),
+                Arguments.arguments(
+                    AuthResult.Error.Register.InvalidEmail,
+                    ScreenState.Error(AuthRegisterUiState.Error(AuthRegisterError.INVALID_EMAIL)),
+                ),
+            )
+        }
+    }
+
+    @Test
+    fun `test resetUiState`() =
+        runTest {
+            coEvery { authRepository.anonymousLogin() } returns AuthResult.Authorized
+
+            authRegisterViewModel.anonymousLogin()
+            authRegisterViewModel.resetUiState()
+
+            authRegisterViewModel.uiState.test {
+                awaitItem() shouldBe ScreenState.Empty
+                awaitItem() shouldBe ScreenState.Loading
+                awaitItem() shouldBeEqualToComparingFields ScreenState.Success(AuthRegisterUiState.Authorized)
+                awaitItem() shouldBe ScreenState.Empty
+                cancelAndIgnoreRemainingEvents()
+            }
+        }
 }
