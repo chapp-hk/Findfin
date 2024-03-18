47b2da48412def4017ecd2c74d368de945dadf6c
chapp <chapp.hkg@gmail.com>
2024-03-18 16:08:00 +0800

    feat(auth-data-remote): add email password register error handling

--- feature/auth/data/remote-firebase/src/main/kotlin/ch/app/hk/bank/locator/feature/auth/data/remote/firebase/service/AuthService.kt
+++ feature/auth/data/remote-firebase/src/main/kotlin/ch/app/hk/bank/locator/feature/auth/data/remote/firebase/service/AuthService.kt
@@ -5,6 +5,7 @@ import ch.app.hk.bank.locator.core.threading.DispatcherIo
 import ch.app.hk.bank.locator.feature.auth.data.remote.datasource.AuthRemoteDataSource
 import ch.app.hk.bank.locator.feature.auth.data.remote.response.AuthResponse
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
@@ -21,23 +22,49 @@ internal class AuthService
             return firebaseAuth.currentUser != null
         }
 
-        override suspend fun anonymousLogin(): Result<AuthResponse> {
+        override suspend fun anonymousLogin(): AuthResponse {
             return withContext(ioDispatcher) {
                 runCatching {
                     val auth = firebaseAuth.signInAnonymously().await()
-                    AuthResponse(auth.user!!.isAnonymous)
+                    AuthResponse.Success(auth.user!!.isAnonymous)
+                }.getOrElse {
+                    AuthResponse.Error(
+                        code = "",
+                        message = "",
+                    )
                 }
             }
         }
 
+        // register test:
+        // - register success
+        // - register with invalid email
+        //   - FirebaseAuthInvalidCredentialsException: ERROR_INVALID_EMAIL - The email address is badly formatted.
+        // - register with short password
+        //   - FirebaseAuthWeakPasswordException: ERROR_WEAK_PASSWORD - The given password is invalid. [ Password should be at least 6 characters ]
+        // - register with existing email
+        //   - FirebaseAuthUserCollisionException: ERROR_EMAIL_ALREADY_IN_USE - The email address is already in use by another account.
+
         override suspend fun emailPasswordRegister(
             email: String,
             password: String,
-        ): Result<AuthResponse> {
+        ): AuthResponse {
             return withContext(ioDispatcher) {
                 runCatching {
-                    val auth = firebaseAuth.createUserWithEmailAndPassword(email, password).await()
-                    AuthResponse(auth.user!!.isAnonymous)
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).await()
+                    AuthResponse.Success(isAnonymous = false)
+                }.getOrElse { error ->
+                    val errorCode =
+                        if (error is FirebaseAuthException) {
+                            error.errorCode
+                        } else {
+                            ""
+                        }
+
+                    AuthResponse.Error(
+                        code = errorCode,
+                        message = "",
+                    )
                 }
             }
         }
