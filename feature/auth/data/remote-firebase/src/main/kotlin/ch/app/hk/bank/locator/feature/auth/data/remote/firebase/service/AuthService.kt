3ee3309b5fa48fddfe605c8fa50529db14bc88ef
chapp <chapp.hkg@gmail.com>
2024-04-06 22:27:05 +0800

    docs(auth-data-remote-firebase): move comments to readme

--- feature/auth/data/remote-firebase/src/main/kotlin/ch/app/hk/bank/locator/feature/auth/data/remote/firebase/service/AuthService.kt
+++ feature/auth/data/remote-firebase/src/main/kotlin/ch/app/hk/bank/locator/feature/auth/data/remote/firebase/service/AuthService.kt
@@ -36,15 +36,6 @@ internal class AuthService
             }
         }
 
-        // register test:
-        // - register success
-        // - register with invalid email
-        //   - FirebaseAuthInvalidCredentialsException: ERROR_INVALID_EMAIL - The email address is badly formatted.
-        // - register with short password
-        //   - FirebaseAuthWeakPasswordException: ERROR_WEAK_PASSWORD - The given password is invalid. [ Password should be at least 6 characters ]
-        // - register with existing email
-        //   - FirebaseAuthUserCollisionException: ERROR_EMAIL_ALREADY_IN_USE - The email address is already in use by another account.
-
         override suspend fun emailPasswordRegister(
             email: String,
             password: String,
