5bf0bb6a442add358255ef613339b8839df481ca
chapp <chapp.hkg@gmail.com>
2024-04-10 22:28:43 +0800

    feat(auth-ui): add login form

--- feature/auth/ui/src/main/kotlin/ch/app/hk/bank/locator/feature/auth/ui/login/view/AuthLogin.kt
+++ feature/auth/ui/src/main/kotlin/ch/app/hk/bank/locator/feature/auth/ui/login/view/AuthLogin.kt
@@ -1,9 +1,39 @@
 package ch.app.hk.bank.locator.feature.auth.ui.login.view
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import ch.app.hk.bank.locator.core.design.ui.text.rememberAppTextFieldState
+import ch.app.hk.bank.locator.feature.auth.ui.R
 
 @Composable
-fun AuthLogin() {
-    Text(text = "login")
+fun AuthLogin(
+    modifier: Modifier = Modifier,
+    onBack: () -> Unit,
+    onAuthorized: () -> Unit,
+) = Box(modifier = modifier) {
+    val emailState =
+        rememberAppTextFieldState(
+            placeholder = stringResource(id = R.string.auth_placeholder_email),
+        )
+    val passwordState =
+        rememberAppTextFieldState(
+            placeholder = stringResource(id = R.string.auth_placeholder_password),
+        )
+    val focusManager = LocalFocusManager.current
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    AuthLoginForm(
+        snackbarHostState = snackbarHostState,
+        emailState = emailState,
+        passwordState = passwordState,
+        onBack = onBack,
+        onLogin = {
+            focusManager.clearFocus(force = true)
+        },
+    )
 }
