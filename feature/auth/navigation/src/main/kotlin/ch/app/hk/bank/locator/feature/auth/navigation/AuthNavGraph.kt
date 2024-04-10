ad827f65a777eea07f4e48dbe120d8d8a63337c7
chapp <chapp.hkg@gmail.com>
2024-04-10 22:04:04 +0800

    feat(auth-ui): add navigation animation

--- feature/auth/navigation/src/main/kotlin/ch/app/hk/bank/locator/feature/auth/navigation/AuthNavGraph.kt
+++ feature/auth/navigation/src/main/kotlin/ch/app/hk/bank/locator/feature/auth/navigation/AuthNavGraph.kt
@@ -1,5 +1,7 @@
 package ch.app.hk.bank.locator.feature.auth.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -24,7 +26,21 @@ fun NavGraphBuilder.authNavGraph(
             )
         }
 
-        composable(route = AuthLoginDestination.route) {
+        composable(
+            route = AuthLoginDestination.route,
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Start,
+                    animationSpec = tween(durationMillis = 300),
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.End,
+                    animationSpec = tween(durationMillis = 300),
+                )
+            },
+        ) {
             AuthLogin()
         }
     }
