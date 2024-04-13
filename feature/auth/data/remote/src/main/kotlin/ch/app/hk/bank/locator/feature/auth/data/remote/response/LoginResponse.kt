48973d95a7d0b6f7cef08a6544215ddb86126e15
chapp <chapp.hkg@gmail.com>
2024-04-13 16:15:12 +0800

    feat(auth-data-remote): add email password login function

---
+++ feature/auth/data/remote/src/main/kotlin/ch/app/hk/bank/locator/feature/auth/data/remote/response/LoginResponse.kt
@@ -0,0 +1,15 @@
+package ch.app.hk.bank.locator.feature.auth.data.remote.response
+
+sealed interface LoginResponse {
+    data object Success : LoginResponse
+
+    sealed interface Error : LoginResponse {
+        data object Unknown : Error
+
+        data object InvalidCredential : Error
+
+        data object AccountDisabled : Error
+
+        data object TooManyRequest : Error
+    }
+}
