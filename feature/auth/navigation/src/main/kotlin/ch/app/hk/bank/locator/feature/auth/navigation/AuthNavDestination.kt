25ddf0e3b9e9a1b2c2cd0cc6e71c01cefbc41ee5
chapp <chapp.hkg@gmail.com>
2024-04-30 23:49:32 +0800

    feat(auth-navigation): add login navigation

--- feature/auth/navigation/src/main/kotlin/ch/app/hk/bank/locator/feature/auth/navigation/AuthNavDestination.kt
+++ feature/auth/navigation/src/main/kotlin/ch/app/hk/bank/locator/feature/auth/navigation/AuthNavDestination.kt
@@ -3,13 +3,18 @@ package ch.app.hk.bank.locator.feature.auth.navigation
 import ch.app.hk.bank.locator.core.navigation.Destination
 
 object AuthNavGraphDestination : Destination() {
-    override val navGraphId: String = "auth-destination"
+    override val navGraphId: String = "auth-initialize-destination"
     override val route: String = "auth-route"
 }
 
 object AuthEntryDestination : Destination() {
     override val navGraphId: String = ""
     override val route: String = "auth-entry-route"
+}
+
+object AuthRegisterDestination : Destination() {
+    override val navGraphId: String = ""
+    override val route: String = "auth-register-route"
 }
 
 object AuthLoginDestination : Destination() {
