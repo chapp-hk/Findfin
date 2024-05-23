d0c2efbcb03ceeea1a2189cd218d26df277990d7
chapp <chapp.hkg@gmail.com>
2024-05-23 23:51:03 +0800

    feat(home): add search bar to home container

--- feature/home/ui/src/main/kotlin/ch/app/hk/bank/locator/feature/home/ui/container/view/HomeContainer.kt
+++ feature/home/ui/src/main/kotlin/ch/app/hk/bank/locator/feature/home/ui/container/view/HomeContainer.kt
@@ -1,9 +1,29 @@
 package ch.app.hk.bank.locator.feature.home.ui.container.view
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import ch.app.hk.bank.locator.core.design.ui.search.AppSearchBar
+import ch.app.hk.bank.locator.core.design.ui.search.rememberAppSearchBarState
+import ch.app.hk.bank.locator.feature.home.ui.R
 import ch.app.hk.bank.locator.feature.home.ui.user.view.UserStatus
 
 @Composable
-internal fun HomeContainer(onRequestAuth: () -> Unit) {
-    UserStatus(onRequestAuth = onRequestAuth)
+internal fun HomeContainer(
+    onRequestAuth: () -> Unit,
+    onSearch: (String) -> Unit,
+) {
+    Column {
+        val searchBarState =
+            rememberAppSearchBarState(
+                placeholder = stringResource(id = R.string.home_placeholder_search),
+            )
+
+        UserStatus(onRequestAuth = onRequestAuth)
+
+        AppSearchBar(
+            state = searchBarState,
+            onSearch = onSearch,
+        )
+    }
 }
