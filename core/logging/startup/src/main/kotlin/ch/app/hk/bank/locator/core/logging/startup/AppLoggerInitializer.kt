0773b696da19e9a35e635d8483946318c2b2253f
chapp <chapp.hkg@gmail.com>
2024-06-03 17:42:51 +0800

    feat(core-logging): add logging startup module

---
+++ core/logging/startup/src/main/kotlin/ch/app/hk/bank/locator/core/logging/startup/AppLoggerInitializer.kt
@@ -0,0 +1,23 @@
+package ch.app.hk.bank.locator.core.logging.startup
+
+import android.content.Context
+import androidx.startup.Initializer
+import co.touchlab.kermit.DefaultFormatter
+import co.touchlab.kermit.Logger
+import co.touchlab.kermit.platformLogWriter
+
+class AppLoggerInitializer : Initializer<Logger> {
+    override fun create(context: Context): Logger {
+        if (BuildConfig.DEBUG) {
+            Logger.setLogWriters(platformLogWriter(DefaultFormatter))
+        } else {
+            Logger.setLogWriters()
+        }
+
+        return Logger
+    }
+
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> {
+        return mutableListOf()
+    }
+}
