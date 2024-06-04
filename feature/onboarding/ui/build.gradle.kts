f4811051be5a1edc5a25e10c38b69741954dde9e
chapp <chapp.hkg@gmail.com>
2024-06-04 15:47:28 +0800

    refactor(core-location): move code in location to impl submodule

--- feature/onboarding/ui/build.gradle.kts
+++ feature/onboarding/ui/build.gradle.kts
@@ -30,7 +30,6 @@ dependencies {
     implementation(projects.core.design.ui)
     implementation(projects.core.preferences.api)
     implementation(projects.core.preferences.impl)
-    implementation(projects.core.location)
     implementation(projects.core.locale.api)
     implementation(projects.core.locale.impl)
     implementation(projects.feature.locator.data.localDatabase)
