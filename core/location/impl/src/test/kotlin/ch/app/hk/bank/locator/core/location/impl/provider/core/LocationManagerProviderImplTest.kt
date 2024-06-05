877b2b31b222ea78285a39dc7b42cd246240f531
chapp <chapp.hkg@gmail.com>
2024-06-05 18:11:11 +0800

    feat(core-location): add LocationManager provider

---
+++ core/location/impl/src/test/kotlin/ch/app/hk/bank/locator/core/location/impl/provider/core/LocationManagerProviderImplTest.kt
@@ -0,0 +1,79 @@
+package ch.app.hk.bank.locator.core.location.impl.provider.core
+
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import io.kotest.matchers.shouldBe
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.lang.Thread.sleep
+
+class LocationManagerProviderImplTest {
+    private val testDispatcher = StandardTestDispatcher()
+    private val context = mockk<Context>()
+    private val locationManager = mockk<LocationManager>()
+
+    private val locationManagerDataSource =
+        LocationManagerProviderImpl(
+            ioDispatcher = testDispatcher,
+            context = context,
+        )
+
+    @BeforeEach
+    fun setUp() {
+        every { context.getSystemService(LocationManager::class.java) } returns locationManager
+    }
+
+    @Test
+    fun `getCurrentLocation returns expected location`() =
+        runTest(testDispatcher) {
+            val expectedLocation = mockk<Location>()
+            coEvery {
+                locationManager.requestLocationUpdates(
+                    any<String>(),
+                    any<Long>(),
+                    any<Float>(),
+                    any<LocationListener>(),
+                )
+            } answers {
+                arg<LocationListener>(3).onLocationChanged(expectedLocation)
+            }
+
+            val result = locationManagerDataSource.getSingleCurrentLocation()
+
+            result shouldBe expectedLocation
+        }
+
+    @Test
+    fun `getCurrentLocation returns null when no location update is received within 5 seconds`() =
+        runTest(testDispatcher) {
+            coEvery {
+                locationManager.requestLocationUpdates(
+                    any<String>(),
+                    any<Long>(),
+                    any<Float>(),
+                    any<LocationListener>(),
+                )
+            } answers {
+                sleep(5000)
+            }
+
+            every {
+                locationManager.removeUpdates(any<LocationListener>())
+            } just Runs
+
+            val result = locationManagerDataSource.getSingleCurrentLocation()
+
+            result shouldBe null
+            verify { locationManager.removeUpdates(any<LocationListener>()) }
+        }
+}
