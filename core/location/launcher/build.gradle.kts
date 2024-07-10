ed7a40f5fc5256be353dace708da3f06f250077f
chapp <chapp.hkg@gmail.com>
2024-07-10 15:17:34 +0800

    test(core-location-launcher): add test runner dependency

--- core/location/launcher/build.gradle.kts
+++ core/location/launcher/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.compose)
 
+    androidTestImplementation(libs.androidx.test.runner)
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.test.espresso.intents)
     androidTestImplementation(libs.mockk.android)
