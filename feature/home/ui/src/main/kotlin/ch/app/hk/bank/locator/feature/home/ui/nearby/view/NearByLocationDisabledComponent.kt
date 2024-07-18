6b6ef65fcc1fb870d5b8e85762d14656379e1276
chapp <chapp.hkg@gmail.com>
2024-07-18 23:44:00 +0800

    feat(home-ui): update near by location disable ui with icon

--- feature/home/ui/src/main/kotlin/ch/app/hk/bank/locator/feature/home/ui/nearby/view/NearByLocationDisabledComponent.kt
+++ feature/home/ui/src/main/kotlin/ch/app/hk/bank/locator/feature/home/ui/nearby/view/NearByLocationDisabledComponent.kt
@@ -3,16 +3,19 @@ package ch.app.hk.bank.locator.feature.home.ui.nearby.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import ch.app.hk.bank.locator.core.location.launcher.setting.rememberLocationSourceSettingsLauncher
+import ch.app.hk.bank.locator.feature.home.ui.R
 
 @Composable
 internal fun NearByLocationDisabledComponent(onLocationServiceEnabled: () -> Unit) {
@@ -31,14 +34,14 @@ internal fun NearByLocationDisabledComponent(onLocationServiceEnabled: () -> Uni
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(
-            text = "Location service disabled",
-            color = MaterialTheme.colorScheme.error,
-            style = MaterialTheme.typography.titleSmall,
+        Icon(
+            modifier = Modifier.size(60.dp),
+            imageVector = ImageVector.vectorResource(id = R.drawable.home_ic_location_disabled),
+            contentDescription = "",
         )
 
         OutlinedButton(
-            modifier = Modifier.padding(top = 8.dp),
+            modifier = Modifier,
             onClick = { launcher.launch(Unit) },
         ) {
             Text(text = "Go settings")
