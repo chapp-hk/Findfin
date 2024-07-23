4379d6dd037266988c4a38a724526db4440f3acf
chapp <chapp.hkg@gmail.com>
2024-07-23 23:27:13 +0800

    feat(home-ui): update location service disabled ui

--- feature/home/ui/src/main/kotlin/ch/app/hk/bank/locator/feature/home/ui/nearby/view/NearByLocationDisabledComponent.kt
+++ feature/home/ui/src/main/kotlin/ch/app/hk/bank/locator/feature/home/ui/nearby/view/NearByLocationDisabledComponent.kt
@@ -3,9 +3,11 @@ package ch.app.hk.bank.locator.feature.home.ui.nearby.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import ch.app.hk.bank.locator.core.location.launcher.setting.rememberLocationSourceSettingsLauncher
 import ch.app.hk.bank.locator.feature.home.ui.R
@@ -37,11 +40,25 @@ internal fun NearByLocationDisabledComponent(onLocationServiceEnabled: () -> Uni
         Icon(
             modifier = Modifier.size(60.dp),
             imageVector = ImageVector.vectorResource(id = R.drawable.home_ic_location_disabled),
+            tint = MaterialTheme.colorScheme.outline,
             contentDescription = "",
         )
 
+        Text(
+            modifier =
+                Modifier.padding(
+                    start = 32.dp,
+                    end = 32.dp,
+                    top = 24.dp,
+                ),
+            text = "Please enable device location service to find nearby services",
+            style = MaterialTheme.typography.labelMedium,
+            color = MaterialTheme.colorScheme.outline,
+            textAlign = TextAlign.Center,
+        )
+
         OutlinedButton(
-            modifier = Modifier,
+            modifier = Modifier.padding(top = 16.dp),
             onClick = { launcher.launch(Unit) },
         ) {
             Text(text = "Go settings")
