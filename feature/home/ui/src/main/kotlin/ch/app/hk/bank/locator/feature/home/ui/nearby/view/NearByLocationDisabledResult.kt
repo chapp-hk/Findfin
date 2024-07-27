53221e0d49548aa01c1549c42d22985dc27b3055
chapp <chapp.hkg@gmail.com>
2024-07-27 14:20:39 +0800

    feat(core-design-ui): add result layout

--- feature/home/ui/src/main/kotlin/ch/app/hk/bank/locator/feature/home/ui/nearby/view/NearByLocationDisabledResult.kt
+++ feature/home/ui/src/main/kotlin/ch/app/hk/bank/locator/feature/home/ui/nearby/view/NearByLocationDisabledResult.kt
@@ -2,26 +2,14 @@ package ch.app.hk.bank.locator.feature.home.ui.nearby.view
 
 import android.content.res.Configuration
 import android.graphics.Color
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import ch.app.hk.bank.locator.core.design.ui.AppContent
+import ch.app.hk.bank.locator.core.design.ui.result.ResultLayout
 import ch.app.hk.bank.locator.core.location.launcher.setting.rememberLocationSourceSettingsLauncher
 import ch.app.hk.bank.locator.feature.home.ui.R
 
@@ -47,43 +35,13 @@ internal fun NearByLocationDisabledResult(
 private fun NearByLocationDisabled(
     modifier: Modifier = Modifier,
     onActionButtonClick: () -> Unit,
-) {
-    Column(
-        modifier =
-            modifier
-                .fillMaxSize()
-                .wrapContentSize(Alignment.Center),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Icon(
-            modifier = Modifier.size(60.dp),
-            imageVector = ImageVector.vectorResource(id = R.drawable.home_ic_location_disabled),
-            tint = MaterialTheme.colorScheme.outline,
-            contentDescription = "",
-        )
-
-        Text(
-            modifier =
-                Modifier.padding(
-                    start = 32.dp,
-                    end = 32.dp,
-                    top = 24.dp,
-                ),
-            text = stringResource(id = R.string.home_label_nearby_location_disabled),
-            style = MaterialTheme.typography.labelMedium,
-            color = MaterialTheme.colorScheme.outline,
-            textAlign = TextAlign.Center,
-        )
-
-        OutlinedButton(
-            modifier = Modifier.padding(top = 16.dp),
-            onClick = onActionButtonClick,
-        ) {
-            Text(text = stringResource(id = R.string.home_button_location_settings))
-        }
-    }
-}
+) = ResultLayout(
+    modifier = modifier,
+    icon = ImageVector.vectorResource(id = R.drawable.home_ic_location_disabled),
+    message = stringResource(id = R.string.home_label_nearby_location_disabled),
+    buttonText = stringResource(id = R.string.home_button_location_settings),
+    onActionButtonClick = onActionButtonClick,
+)
 
 @Preview(
     uiMode = Configuration.UI_MODE_NIGHT_NO,
