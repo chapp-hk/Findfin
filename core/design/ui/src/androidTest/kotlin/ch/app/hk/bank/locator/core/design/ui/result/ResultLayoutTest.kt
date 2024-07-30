edda34512e3bd5d8b28807231e1cbbac950b42ee
chapp <chapp.hkg@gmail.com>
2024-07-30 16:49:49 +0800

    feat(core-design-ui): add logic to hide button in result layout

---
+++ core/design/ui/src/androidTest/kotlin/ch/app/hk/bank/locator/core/design/ui/result/ResultLayoutTest.kt
@@ -0,0 +1,85 @@
+package ch.app.hk.bank.locator.core.design.ui.result
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import ch.app.hk.bank.locator.core.design.ui.AppContent
+import ch.app.hk.bank.locator.core.design.ui.test.R
+import org.junit.Rule
+import org.junit.Test
+
+class ResultLayoutTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun resultLayout_displaysButtonWhenButtonTextAndOnClickProvided() {
+        composeTestRule.setContent {
+            AppContent {
+                ResultLayout(
+                    icon = ImageVector.vectorResource(id = R.drawable.core_ui_ic_android),
+                    message = "Message",
+                    buttonText = "Button",
+                    onActionButtonClick = { },
+                )
+            }
+        }
+
+        composeTestRule
+            .onNodeWithText("Button")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun resultLayout_doesNotDisplayButtonWhenOnlyButtonTextProvided() {
+        composeTestRule.setContent {
+            AppContent {
+                ResultLayout(
+                    icon = ImageVector.vectorResource(id = R.drawable.core_ui_ic_android),
+                    message = "Message",
+                    buttonText = "Button",
+                )
+            }
+        }
+
+        composeTestRule
+            .onNodeWithText("Button")
+            .assertIsNotDisplayed()
+    }
+
+    @Test
+    fun resultLayout_doesNotDisplayButtonWhenOnlyOnClickProvided() {
+        composeTestRule.setContent {
+            AppContent {
+                ResultLayout(
+                    icon = ImageVector.vectorResource(id = R.drawable.core_ui_ic_android),
+                    message = "Message",
+                    onActionButtonClick = { },
+                )
+            }
+        }
+
+        composeTestRule
+            .onNodeWithText("Button")
+            .assertIsNotDisplayed()
+    }
+
+    @Test
+    fun resultLayout_doesNotDisplayButtonWhenNoButtonTextOrOnClickProvided() {
+        composeTestRule.setContent {
+            AppContent {
+                ResultLayout(
+                    icon = ImageVector.vectorResource(id = R.drawable.core_ui_ic_android),
+                    message = "Message",
+                )
+            }
+        }
+
+        composeTestRule
+            .onNodeWithText("Button")
+            .assertIsNotDisplayed()
+    }
+}
