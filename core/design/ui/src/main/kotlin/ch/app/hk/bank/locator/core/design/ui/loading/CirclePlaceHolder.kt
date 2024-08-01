110672f7635c35cb0ed6edbb561a10d541109fbc
chapp <chapp.hkg@gmail.com>
2024-08-01 17:15:27 +0800

    chore(core-design-ui): update place holder background color

--- core/design/ui/src/main/kotlin/ch/app/hk/bank/locator/core/design/ui/loading/CirclePlaceHolder.kt
+++ core/design/ui/src/main/kotlin/ch/app/hk/bank/locator/core/design/ui/loading/CirclePlaceHolder.kt
@@ -1,14 +1,12 @@
 package ch.app.hk.bank.locator.core.design.ui.loading
 
 import android.content.res.Configuration
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -21,7 +19,6 @@ fun CirclePlaceHolder(
     modifier =
         modifier
             .clip(shape = CircleShape)
-            .background(color = Color.LightGray)
             .size(size)
             .shimmer(),
 )
