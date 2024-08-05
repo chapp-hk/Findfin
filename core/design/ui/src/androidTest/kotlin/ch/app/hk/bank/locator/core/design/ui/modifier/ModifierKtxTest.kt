277d6fc9595a9d0a0a95211399df427add2ab0f5
chapp <chapp.hkg@gmail.com>
2024-08-05 16:42:24 +0800

    feat(core-design-ui): add content description modifier extension

---
+++ core/design/ui/src/androidTest/kotlin/ch/app/hk/bank/locator/core/design/ui/modifier/ModifierKtxTest.kt
@@ -0,0 +1,28 @@
+package ch.app.hk.bank.locator.core.design.ui.modifier
+
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import org.junit.Rule
+import org.junit.Test
+
+class ModifierKtxTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun contentDescription_isDisplayedCorrectly() {
+        composeTestRule.setContent {
+            Text(
+                modifier = Modifier.contentDescription("test content description"),
+                text = "test modifier",
+            )
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription("test content description")
+            .assertIsDisplayed()
+    }
+}
