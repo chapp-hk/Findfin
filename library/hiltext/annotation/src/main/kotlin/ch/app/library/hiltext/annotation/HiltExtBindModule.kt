036ac0ba0ce02fd9fa4587c2cc930865788567a5
chapp <chapp.hkg@gmail.com>
2024-08-14 23:00:52 +0800

    docs(library-hiltext): add docs for annotations

--- library/hiltext/annotation/src/main/kotlin/ch/app/library/hiltext/annotation/HiltExtBindModule.kt
+++ library/hiltext/annotation/src/main/kotlin/ch/app/library/hiltext/annotation/HiltExtBindModule.kt
@@ -2,6 +2,14 @@ package ch.app.library.hiltext.annotation
 
 import kotlin.reflect.KClass
 
+/**
+ * Annotation to mark a class as a Hilt module for binding dependencies.
+ *
+ * This annotation is used to generate Hilt modules that bind a specific type to its implementation.
+ *
+ * @property superType The super type or interface that the annotated class will bind to.
+ * @property component The Hilt component in which the module will be installed.
+ */
 @Target(AnnotationTarget.CLASS)
 annotation class HiltExtBindModule(
     val superType: KClass<*> = Nothing::class,
