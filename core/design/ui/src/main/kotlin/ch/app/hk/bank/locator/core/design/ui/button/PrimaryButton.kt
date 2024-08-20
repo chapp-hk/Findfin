84851d2ad1f1f1679011b913a6d1bfb1217dfb87
chapp <chapp.hkg@gmail.com>
2024-08-20 18:57:09 +0800

    feat(core-design-ui): add buttons preview

--- core/design/ui/src/main/kotlin/ch/app/hk/bank/locator/core/design/ui/button/PrimaryButton.kt
+++ core/design/ui/src/main/kotlin/ch/app/hk/bank/locator/core/design/ui/button/PrimaryButton.kt
@@ -1,9 +1,13 @@
 package ch.app.hk.bank.locator.core.design.ui.button
 
+import android.content.res.Configuration
+import android.graphics.Color
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ch.app.hk.bank.locator.core.design.ui.AppContent
 
 @Composable
 fun PrimaryButton(
@@ -15,4 +19,21 @@ fun PrimaryButton(
     onClick = onClick,
 ) {
     Text(text = text)
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    showBackground = true,
+    backgroundColor = Color.WHITE.toLong(),
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    backgroundColor = Color.BLACK.toLong(),
+)
+@Composable
+private fun PrimaryButtonPreview() {
+    AppContent {
+        PrimaryButton(text = "Primary Button", onClick = {})
+    }
 }
--- core/design/ui/src/main/kotlin/ch/app/hk/bank/locator/core/design/ui/button/TertiaryButton.kt
+++ core/design/ui/src/main/kotlin/ch/app/hk/bank/locator/core/design/ui/button/TertiaryButton.kt
@@ -1,9 +1,13 @@
 package ch.app.hk.bank.locator.core.design.ui.button
 
+import android.content.res.Configuration
+import android.graphics.Color
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ch.app.hk.bank.locator.core.design.ui.AppContent
 
 @Composable
 fun TertiaryButton(
@@ -15,4 +19,21 @@ fun TertiaryButton(
     onClick = onClick,
 ) {
     Text(text = text)
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    showBackground = true,
+    backgroundColor = Color.WHITE.toLong(),
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    backgroundColor = Color.BLACK.toLong(),
+)
+@Composable
+private fun TertiaryButtonPreview() {
+    AppContent {
+        TertiaryButton(text = "Tertiary Button", onClick = {})
+    }
 }
