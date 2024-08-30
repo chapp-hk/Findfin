b2695244884ac7d02858660776af75687cf0700e
chapp <chapp.hkg@gmail.com>
2024-08-30 18:56:37 +0800

    feat(locator-navigation): add locator navigation module

---
+++ feature/locator/navigation/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.app.android.common)
+    alias(libs.plugins.app.compose)
+}
+
+android {
+    namespace = "ch.app.hk.bank.locator.feature.locator.navigation"
+
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+}
+
+dependencies {
+    implementation(projects.feature.locator.ui)
+
+    implementation(libs.androidx.navigation.compose)
+}
---
+++ feature/locator/navigation/src/main/kotlin/ch/app/hk/bank/locator/feature/locator/navigation/LocatorDestination.kt
@@ -0,0 +1,9 @@
+package ch.app.hk.bank.locator.feature.locator.navigation
+
+import androidx.compose.runtime.Composable
+import ch.app.hk.bank.locator.feature.locator.ui.map.view.MapScreen
+
+@Composable
+fun MapDestination() {
+    MapScreen()
+}
