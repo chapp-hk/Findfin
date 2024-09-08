45df81182078986e1ab97a809712b426ca3c114a
chapp <chapp.hkg@gmail.com>
2024-09-08 13:37:25 +0800

    docs(locator-navigation): add kdoc

--- feature/locator/navigation/src/main/kotlin/ch/app/hk/bank/locator/feature/locator/navigation/LocatorDestination.kt
+++ feature/locator/navigation/src/main/kotlin/ch/app/hk/bank/locator/feature/locator/navigation/LocatorDestination.kt
@@ -5,11 +5,17 @@ import ch.app.hk.bank.locator.core.navigation.BottomNavigationTab
 import ch.app.hk.bank.locator.feature.locator.ui.R
 import ch.app.hk.bank.locator.feature.locator.ui.map.view.MapScreen
 
+/**
+ * Composable function that displays the map screen.
+ */
 @Composable
 fun MapDestination() {
     MapScreen()
 }
 
+/**
+ * Represents the bottom navigation tab for the map screen.
+ */
 val mapBottomTabDestination =
     BottomNavigationTab(
         route = "home-bottom-map",
