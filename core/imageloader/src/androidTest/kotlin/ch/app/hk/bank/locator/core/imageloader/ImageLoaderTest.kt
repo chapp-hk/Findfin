25cb87944e738e164128152e5dcca81a61fac0e4
chapp <chapp.hkg@gmail.com>
2024-09-08 23:09:21 +0800

    test(core-imageloader): add image view ui test

---
+++ core/imageloader/src/androidTest/kotlin/ch/app/hk/bank/locator/core/imageloader/ImageLoaderTest.kt
@@ -0,0 +1,28 @@
+package ch.app.hk.bank.locator.core.imageloader
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import ch.app.hk.bank.locator.core.imageloader.test.R
+import org.junit.Rule
+import org.junit.Test
+
+class ImageLoaderTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun testImageView() {
+        composeTestRule.setContent {
+            ImageView(
+                source = R.drawable.core_imageloader_error,
+                contentDescription = "test image loader",
+            )
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription("test image loader")
+            .assertIsDisplayed()
+    }
+}
