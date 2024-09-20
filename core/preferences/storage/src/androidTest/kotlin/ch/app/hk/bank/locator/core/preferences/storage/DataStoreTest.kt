5b2810e219e9d12a4c06c09576feb4109c631d77
chapp <chapp.hkg@gmail.com>
2024-09-20 14:59:52 +0800

    test(core-preferences-storage): add datastore unit tests

---
+++ core/preferences/storage/src/androidTest/kotlin/ch/app/hk/bank/locator/core/preferences/storage/DataStoreTest.kt
@@ -0,0 +1,17 @@
+package ch.app.hk.bank.locator.core.preferences.storage
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.test.core.app.ApplicationProvider
+import io.kotest.matchers.types.shouldBeInstanceOf
+import org.junit.Test
+
+class DataStoreTest {
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+
+    @Test
+    fun testDataStoreType() {
+        context.dataStore.shouldBeInstanceOf<DataStore<Preferences>>()
+    }
+}
