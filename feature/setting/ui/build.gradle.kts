e2f4fdd3f527d80bbd1958df9a5e18897312202b
chapp <chapp.hkg@gmail.com>
2024-09-20 16:32:53 +0800

    feat(setting-ui): add theme preference to setting list screen

--- feature/setting/ui/build.gradle.kts
+++ feature/setting/ui/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.app.android.common)
     alias(libs.plugins.app.compose)
+    alias(libs.plugins.app.hilt.android)
+    alias(libs.plugins.app.hilt.android.test)
 }
 
 android {
@@ -15,6 +17,21 @@ android {
 }
 
 dependencies {
+    implementation(projects.core.preferences.uiFoundation)
+    implementation(projects.feature.setting.data.repo)
+    implementation(projects.feature.setting.data.localPreferences)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material)
+
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.kotest.assertions.core)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.turbine)
+
+    androidTestImplementation(libs.androidx.test.core)
+    androidTestImplementation(libs.kotest.assertions.core)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
