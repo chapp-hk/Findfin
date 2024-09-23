66cbf8515cd4aa21a25538c133025c4bf355a2d4
chapp <chapp.hkg@gmail.com>
2024-09-23 14:16:38 +0800

    build(app): bump targetSdk version to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "ch.app.hk.bank.locator"
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
     }
