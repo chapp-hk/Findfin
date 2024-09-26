d17bfc6393366a1c2f3c25264a65bec87d1b4b3f
chapp <chapp.hkg@gmail.com>
2024-09-26 15:04:12 +0800

    feat(home-ui): add current job cancellation logic for new launch

--- feature/home/ui/src/main/kotlin/ch/app/hk/bank/locator/feature/home/ui/nearby/viewmodel/NearByViewModelImpl.kt
+++ feature/home/ui/src/main/kotlin/ch/app/hk/bank/locator/feature/home/ui/nearby/viewmodel/NearByViewModelImpl.kt
@@ -7,6 +7,7 @@ import ch.app.hk.bank.locator.feature.home.domain.nearby.usecase.GetNearByServic
 import ch.app.hk.bank.locator.feature.home.ui.nearby.model.NearByItemUiModel
 import ch.app.hk.bank.locator.feature.home.ui.nearby.model.NearByUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -16,6 +17,7 @@ import javax.inject.Inject
 class NearByViewModelImpl @Inject constructor(
     private val getNearByBanks: GetNearByServicesUseCase,
 ) : NearByViewModel, ViewModel() {
+    private var currentJob: Job? = null
     private val _uiState = MutableStateFlow<NearByUiState>(NearByUiState.Loading)
     override val uiState = _uiState.asStateFlow()
 
@@ -24,27 +26,29 @@ class NearByViewModelImpl @Inject constructor(
     }
 
     override fun getNearByServices() {
-        viewModelScope.launch {
-            when (val result = getNearByBanks()) {
-                is NearByResult.UnknownError ->
-                    _uiState.emit(NearByUiState.Error)
+        currentJob?.cancel()
+        currentJob =
+            viewModelScope.launch {
+                when (val result = getNearByBanks()) {
+                    is NearByResult.UnknownError ->
+                        _uiState.emit(NearByUiState.Error)
 
-                is NearByResult.Location -> {
-                    if (result.list.isEmpty()) {
-                        _uiState.emit(NearByUiState.Empty)
-                    } else {
-                        val itemList =
-                            result.list.map {
-                                NearByItemUiModel(
-                                    name = it.bankName,
-                                    address = it.address,
-                                    isFavourite = false,
-                                )
-                            }
-                        _uiState.emit(NearByUiState.Service(itemList))
+                    is NearByResult.Location -> {
+                        if (result.list.isEmpty()) {
+                            _uiState.emit(NearByUiState.Empty)
+                        } else {
+                            val itemList =
+                                result.list.map {
+                                    NearByItemUiModel(
+                                        name = it.bankName,
+                                        address = it.address,
+                                        isFavourite = false,
+                                    )
+                                }
+                            _uiState.emit(NearByUiState.Service(itemList))
+                        }
                     }
                 }
             }
-        }
     }
 }
