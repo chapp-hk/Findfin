4ebdd7491bf3b67d7ec1c83814eee37ed99aec3b
chapp <chapp.hkg@gmail.com>
2024-09-26 18:34:07 +0800

    chore: temp change

--- feature/home/ui/src/androidTest/kotlin/ch/app/hk/bank/locator/feature/home/ui/container/view/HomeContainerContentTest.kt
+++ feature/home/ui/src/androidTest/kotlin/ch/app/hk/bank/locator/feature/home/ui/container/view/HomeContainerContentTest.kt
@@ -1,13 +1,18 @@
 package ch.app.hk.bank.locator.feature.home.ui.container.view
 
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import ch.app.hk.bank.locator.feature.home.ui.R
 import ch.app.hk.bank.locator.feature.home.ui.nearby.viewmodel.NearByViewModel
 import ch.app.hk.bank.locator.testing.instrument.HiltComponentActivity
+import ch.app.hk.bank.locator.testing.instrument.getResourceString
 import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import io.mockk.mockk
 import org.junit.Rule
+import org.junit.Test
 
 @HiltAndroidTest
 class HomeContainerContentTest {
@@ -20,30 +25,27 @@ class HomeContainerContentTest {
     @BindValue
     val nearByViewModel = mockk<NearByViewModel>()
 
-//    @Test
-//    fun homeContainerContent_displaysAppSearchBarAndFinding() {
-//        // Set up the mock function
-//        val onSearch: (String) -> Unit = {}
-//        every { nearByViewModel.uiState } returns
-//            MutableStateFlow(ScreenState.Error(NearByUiState.Error(NearByError.GPS_IS_OFF)))
-//
-//        // Start the HomeContainerContent composable
-//        composeTestRule.setContent {
-//            HomeContainerContent(
-//                nearByViewModel = nearByViewModel,
-//                onSearch = onSearch,
-//            )
-//        }
-//
-//        // Check if AppSearchBar and Finding are displayed
-//        composeTestRule
-//            .onNodeWithContentDescription(getResourceString(R.string.home_content_description_search))
-//            .assertIsDisplayed()
-//
-//        composeTestRule
-//            .onNodeWithContentDescription(getResourceString(R.string.home_content_description_finding))
-//            .assertIsDisplayed()
-//    }
+    @Test
+    fun homeContainerContent_displaysAppSearchBarAndFinding() {
+        // Set up the mock function
+        val onSearch: (String) -> Unit = {}
+
+        // Start the HomeContainerContent composable
+        composeTestRule.setContent {
+            HomeContainerContent(
+                onSearch = onSearch,
+            )
+        }
+
+        // Check if AppSearchBar and Finding are displayed
+        composeTestRule
+            .onNodeWithContentDescription(getResourceString(R.string.home_content_description_search))
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithContentDescription(getResourceString(R.string.home_content_description_finding))
+            .assertIsDisplayed()
+    }
 //
 //    @Test
 //    fun homeContainerContent_displaysLoadingIndicator() {
@@ -64,9 +66,9 @@ class HomeContainerContentTest {
 //            .onNodeWithContentDescription(getResourceString(R.string.home_content_description_loading))
 //            .assertIsDisplayed()
 //    }
-//
-//    @Test
-//    fun homeContainerContent_displaysNearByLocationDisabledResult() {
+
+    @Test
+    fun homeContainerContent_displaysNearByLocationDisabledResult() {
 //        // Set up the mock function
 //        every { nearByViewModel.uiState } returns
 //            MutableStateFlow(ScreenState.Error(NearByUiState.Error(NearByError.GPS_IS_OFF)))
@@ -74,7 +76,6 @@ class HomeContainerContentTest {
 //        // Start the HomeContainerContent composable
 //        composeTestRule.setContent {
 //            HomeContainerContent(
-//                nearByViewModel = nearByViewModel,
 //                onSearch = {},
 //            )
 //        }
@@ -83,7 +84,7 @@ class HomeContainerContentTest {
 //        composeTestRule
 //            .onNodeWithContentDescription(getResourceString(R.string.home_content_description_location_disabled))
 //            .assertIsDisplayed()
-//    }
+    }
 //
 //    @Test
 //    fun homeContainerContent_displaysLocationPermissionDeniedResult() {
