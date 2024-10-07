447ca3d641bd0f61877df12d9a99a20345a7923f
chapp <chapp.hkg@gmail.com>
2024-10-07 10:08:02 +0800

    chore(core-map): move classes to new package

--- feature/locator/ui/src/main/kotlin/org/chapp/findfin/feature/locator/ui/map/view/MapScreen.kt
+++ feature/locator/ui/src/main/kotlin/org/chapp/findfin/feature/locator/ui/map/view/MapScreen.kt
@@ -2,8 +2,12 @@ package org.chapp.findfin.feature.locator.ui.map.view
 
 import androidx.compose.runtime.Composable
 import org.chapp.findfin.core.map.AppMap
+import org.chapp.findfin.core.map.Position
 
 @Composable
 fun MapScreen() {
-    AppMap()
+    AppMap(
+        initPosition = Position(22.3193, 114.1694),
+        initZoom = 17f,
+    )
 }
