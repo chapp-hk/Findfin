1d6ddf30625a8370d8bc96f28f26c85369ed0e3f
chapp <chapp.hkg@gmail.com>
2024-10-10 11:45:07 +0800

    feat(locator-ui): display map markers for all location

--- feature/locator/presentation/src/main/kotlin/org/chapp/findfin/feature/locator/presentation/ui/map/view/MapScreen.kt
+++ feature/locator/presentation/src/main/kotlin/org/chapp/findfin/feature/locator/presentation/ui/map/view/MapScreen.kt
@@ -1,29 +1,31 @@
 package org.chapp.findfin.feature.locator.presentation.ui.map.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.chapp.findfin.core.map.AppMap
-import org.chapp.findfin.core.map.MapMarker
 import org.chapp.findfin.core.map.Position
 import org.chapp.findfin.core.map.rememberAppMapCameraState
+import org.chapp.findfin.feature.locator.ui.map.viewmodel.MapViewModel
 
 @Composable
-internal fun MapScreen() {
+internal fun MapScreen(
+    modifier: Modifier = Modifier,
+    mapViewModel: MapViewModel = hiltViewModel(),
+) {
     val mapCameraState =
         rememberAppMapCameraState(
             initPosition = Position(latitude = 22.3193, longitude = 114.1694),
             initZoom = 10f,
         )
 
+    val mapMarkers by mapViewModel.uiState.collectAsStateWithLifecycle()
+
     AppMap(
+        modifier = modifier,
         cameraState = mapCameraState,
-        markers =
-            (1..100).map { index ->
-                MapMarker(
-                    itemPosition = Position(latitude = 22.3193 + index * 0.001, longitude = 114.1694 + index * 0.001),
-                    itemTitle = "Marker $index",
-                    itemSnippet = "Snippet $index",
-                    itemZIndex = index.toFloat(),
-                )
-            },
+        markers = mapMarkers,
     )
 }
