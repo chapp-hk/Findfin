6e9e19bd38e23fe772d49fe7db5757f0d9b4b115
chapp <chapp.hkg@gmail.com>
2024-10-24 22:47:04 +0800

    feat(setting): add language preference

---
+++ feature/setting/ui/src/main/kotlin/org/chapp/findfin/feature/setting/ui/list/view/LanguagePreference.kt
@@ -0,0 +1,31 @@
+package org.chapp.findfin.feature.setting.ui.list.view
+
+import androidx.compose.runtime.Composable
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import org.chapp.findfin.core.preferences.runtime.PreferenceStore
+import org.chapp.findfin.core.preferences.ui.foundation.ListPreference
+import org.chapp.findfin.core.preferences.ui.foundation.ListPreferenceItem
+
+@Composable
+internal fun LanguagePreference() {
+    ListPreference(
+        title = "Language",
+        list =
+            listOf(
+                ListPreferenceItem(
+                    titleRes = "English",
+                    value = "en",
+                ),
+            ),
+        preferenceStore =
+            object : PreferenceStore<String> {
+                override fun get(): Flow<String> {
+                    return flowOf()
+                }
+
+                override suspend fun set(value: String) {
+                }
+            },
+    )
+}
