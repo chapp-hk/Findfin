0a49604e9047b4c02b66a08493bf5cf5aca4f460
chapp <chapp.hkg@gmail.com>
2024-10-28 22:32:40 +0800

    feat(setting-ui): add language setting item base on android version

--- feature/setting/ui/src/main/kotlin/org/chapp/findfin/feature/setting/ui/list/view/language/LanguagePreference.kt
+++ feature/setting/ui/src/main/kotlin/org/chapp/findfin/feature/setting/ui/list/view/language/LanguagePreference.kt
@@ -1,16 +1,51 @@
 package org.chapp.findfin.feature.setting.ui.list.view.language
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.core.app.LocaleManagerCompat
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import org.chapp.findfin.core.preferences.runtime.PreferenceStore
 import org.chapp.findfin.core.preferences.ui.foundation.ListPreference
 import org.chapp.findfin.core.preferences.ui.foundation.ListPreferenceItem
+import org.chapp.findfin.core.preferences.ui.foundation.Preference
 import org.chapp.findfin.feature.setting.ui.R
 
 @Composable
 internal fun LanguagePreference() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        LanguagePreferenceApi33()
+    } else {
+        LanguagePreferenceLegacy()
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+@Composable
+private fun LanguagePreferenceApi33() {
+    val context = LocalContext.current
+    Preference(
+        modifier =
+            Modifier.clickable {
+                val intent = Intent(Settings.ACTION_APP_LOCALE_SETTINGS)
+                intent.data = Uri.fromParts("package", context.packageName, null)
+                context.startActivity(intent)
+            },
+        title = stringResource(id = R.string.setting_language_title),
+        description = LocaleManagerCompat.getApplicationLocales(context).toLanguageTags(),
+    )
+}
+
+@Composable
+private fun LanguagePreferenceLegacy() {
     ListPreference(
         title = stringResource(id = R.string.setting_language_title),
         list =
