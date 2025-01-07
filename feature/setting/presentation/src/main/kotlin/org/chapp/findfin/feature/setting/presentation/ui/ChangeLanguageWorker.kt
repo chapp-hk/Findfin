ba06662e25963e703b2e55b254c621376b6286ec
chapp <chapp.hkg@gmail.com>
2025-01-07 23:13:23 +0800

    feat(setting): fetch all bank location in change language worker

--- feature/setting/presentation/src/main/kotlin/org/chapp/findfin/feature/setting/presentation/ui/ChangeLanguageWorker.kt
+++ feature/setting/presentation/src/main/kotlin/org/chapp/findfin/feature/setting/presentation/ui/ChangeLanguageWorker.kt
@@ -6,13 +6,16 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import org.chapp.findfin.feature.onboarding.domain.fetch.usecase.FetchAllBankLocationsWithLanguageUseCase
 
 @HiltWorker
 class ChangeLanguageWorker @AssistedInject constructor(
     @Assisted context: Context,
     @Assisted workerParams: WorkerParameters,
+    private val fetchAllBankLocationsWithLanguage: FetchAllBankLocationsWithLanguageUseCase,
 ) : CoroutineWorker(context, workerParams) {
     override suspend fun doWork(): Result {
+        fetchAllBankLocationsWithLanguage("zh")
         return Result.success()
     }
 }
