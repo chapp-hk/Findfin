4efe451e1f98e97b2923191f5e61392c976a5ce9
chapp <chapp.hkg@gmail.com>
2025-03-24 23:24:23 +0800

    test(home): update tests

--- feature/home/presentation/src/test/kotlin/org/chapp/findfin/feature/home/presentation/ui/nearby/viewmodel/NearByViewModelImplTest.kt
+++ feature/home/presentation/src/test/kotlin/org/chapp/findfin/feature/home/presentation/ui/nearby/viewmodel/NearByViewModelImplTest.kt
@@ -4,26 +4,35 @@ import app.cash.turbine.test
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
+import org.chapp.findfin.core.locale.AppLocaleManager
 import org.chapp.findfin.feature.home.domain.nearby.model.NearByResult
 import org.chapp.findfin.feature.home.domain.nearby.model.Service
 import org.chapp.findfin.feature.home.domain.nearby.usecase.GetNearByServicesUseCase
 import org.chapp.findfin.feature.home.presentation.ui.nearby.model.NearByUiState
 import org.chapp.findfin.testing.extension.MainDispatcherExtension
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(MainDispatcherExtension::class)
 class NearByViewModelImplTest {
+    private val appLocaleManager = mockk<AppLocaleManager>()
     private val getNearByServicesUseCase = mockk<GetNearByServicesUseCase>()
+
+    @BeforeEach
+    fun setUp() {
+        every { appLocaleManager.getCurrentLocale() } returns null
+    }
 
     @Test
     fun `uiState emits Service state when use case returns Location list`() {
         runTest {
             val mockLocationList = listOf<Service>(mockk(relaxed = true))
             val mockResult = NearByResult.Location(mockLocationList)
-            coEvery { getNearByServicesUseCase() } returns mockResult
+            coEvery { getNearByServicesUseCase(language = any()) } returns mockResult
 
             val nearByViewModel = createNearByViewModel()
             nearByViewModel.getNearByServices()
@@ -40,7 +49,7 @@ class NearByViewModelImplTest {
     fun `uiState emits Empty state when use case returns empty Location list`() {
         runTest {
             val mockResult = NearByResult.Location(listOf())
-            coEvery { getNearByServicesUseCase() } returns mockResult
+            coEvery { getNearByServicesUseCase(language = any()) } returns mockResult
 
             val nearByViewModel = createNearByViewModel()
             nearByViewModel.getNearByServices()
@@ -56,7 +65,7 @@ class NearByViewModelImplTest {
     @Test
     fun `uiState emits Error state when use case returns UnknownError`() {
         runTest {
-            coEvery { getNearByServicesUseCase() } returns NearByResult.UnknownError
+            coEvery { getNearByServicesUseCase(language = any()) } returns NearByResult.UnknownError
 
             val nearByViewModel = createNearByViewModel()
             nearByViewModel.getNearByServices()
@@ -69,5 +78,9 @@ class NearByViewModelImplTest {
         }
     }
 
-    private fun createNearByViewModel() = NearByViewModelImpl(getNearByServicesUseCase)
+    private fun createNearByViewModel() =
+        NearByViewModelImpl(
+            appLocaleManager = appLocaleManager,
+            getNearByBanks = getNearByServicesUseCase,
+        )
 }
