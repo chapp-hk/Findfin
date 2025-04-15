6a7d4087d9bbf94ddf1c13f517b2089060faf394
chapp <chapp.hkg@gmail.com>
2025-04-15 23:05:18 +0800

    test(core-locale-api): add unit tests

--- core/locale/api/build.gradle.kts
+++ core/locale/api/build.gradle.kts
@@ -2,3 +2,8 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.app.jvm)
 }
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.kotest.assertions.core)
+}
