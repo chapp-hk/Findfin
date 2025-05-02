eb7ede4cdd0a79a84cd9be54d1ae9af9c48c6b5d
chapp <chapp.hkg@gmail.com>
2025-05-02 23:19:24 +0800

    feat(feature-home): create home event data class

--- feature/home/presentation/src/main/kotlin/org/chapp/findfin/feature/home/presentation/ui/container/view/HomeContainer.kt
+++ feature/home/presentation/src/main/kotlin/org/chapp/findfin/feature/home/presentation/ui/container/view/HomeContainer.kt
@@ -2,15 +2,13 @@ package org.chapp.findfin.feature.home.presentation.ui.container.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import org.chapp.findfin.feature.home.presentation.navigation.HomeEvent
 import org.chapp.findfin.feature.home.presentation.ui.user.view.UserStatus
 
 @Composable
-internal fun HomeContainer(
-    onRequestAuth: () -> Unit,
-    onSearch: (String) -> Unit,
-) {
+internal fun HomeContainer(homeEvent: HomeEvent) {
     Column {
-        UserStatus(onRequestAuth = onRequestAuth)
-        HomeContainerContent(onSearch = onSearch)
+        UserStatus(onRequestAuth = homeEvent.onRequestAuth)
+        HomeContainerContent(onSearch = homeEvent.onSearch)
     }
 }
