87c5edfeb94d743f26bb78d3d16a8976fdaeb187
chapp <chapp.hkg@gmail.com>
2025-05-15 18:21:15 +0800

    docs(bank-data): add kdoc for BankFetchResult

--- feature/bank/data/repo/src/main/kotlin/org/chapp/findfin/feature/bank/data/repo/model/BankFetchResult.kt
+++ feature/bank/data/repo/src/main/kotlin/org/chapp/findfin/feature/bank/data/repo/model/BankFetchResult.kt
@@ -1,5 +1,13 @@
 package org.chapp.findfin.feature.bank.data.repo.model
 
+/**
+ * Represents the result of a bank fetch operation.
+ *
+ * This sealed interface defines possible outcomes when fetching banks:
+ * - [HasNext]: Indicates that there are more banks to fetch (pagination continues).
+ * - [End]: Indicates that there are no more banks to fetch (end of pagination).
+ * - [Error]: Indicates that an error occurred during the fetch operation.
+ */
 sealed interface BankFetchResult {
     data object HasNext : BankFetchResult
 
