e357aa70f75fac6f6109b2f799477ed5f2ab38bc
chapp <chapp.hkg@gmail.com>
2025-06-12 15:26:51 +0800

    chore: temp change

--- core/preferences/storage/build.gradle.kts
+++ core/preferences/storage/build.gradle.kts
@@ -11,6 +11,12 @@ android {
     defaultConfig {
         consumerProguardFiles("consumer-rules.pro")
     }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
 }
 
 dependencies {
@@ -22,4 +28,11 @@ dependencies {
     androidTestImplementation(libs.kotest.assertions.core)
     androidTestImplementation(libs.turbine)
     androidTestImplementation(libs.androidx.test.runner)
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.robolectric:robolectric:4.13")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.8.2")
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.kotest.assertions.core)
+    testImplementation(libs.turbine)
 }
