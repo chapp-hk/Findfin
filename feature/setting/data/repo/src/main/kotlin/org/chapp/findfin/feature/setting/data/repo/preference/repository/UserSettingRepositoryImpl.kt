a7aac3c2043675d669299043d042d09ed264a95c
chApp <chapp.hkg@gmail.com>
2025-10-01 00:35:11 +0800

    chore: remove redundant coroutines context switching (#234)

--- feature/setting/data/repo/src/main/kotlin/org/chapp/findfin/feature/setting/data/repo/preference/repository/UserSettingRepositoryImpl.kt
+++ feature/setting/data/repo/src/main/kotlin/org/chapp/findfin/feature/setting/data/repo/preference/repository/UserSettingRepositoryImpl.kt
@@ -1,11 +1,7 @@
 package org.chapp.findfin.feature.setting.data.repo.preference.repository
 
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.withContext
-import org.chapp.findfin.core.threading.DispatcherIo
 import org.chapp.findfin.feature.setting.data.repo.preference.local.datasource.UserSettingLocalDataSource
 import org.chapp.findfin.feature.setting.data.repo.preference.model.Theme
 import org.chapp.library.hiltwrap.annotation.HiltWrapBindModule
@@ -15,31 +11,23 @@ import javax.inject.Singleton
 @Singleton
 @HiltWrapBindModule
 internal class UserSettingRepositoryImpl @Inject constructor(
-    @param:DispatcherIo private val ioDispatcher: CoroutineDispatcher,
     private val userSettingLocalDataSource: UserSettingLocalDataSource,
 ) : UserSettingRepository {
     override suspend fun setLanguagePreference(language: String) {
-        withContext(ioDispatcher) {
-            userSettingLocalDataSource.setLanguagePreference(language)
-        }
+        userSettingLocalDataSource.setLanguagePreference(language)
     }
 
     override fun getLanguagePreference(): Flow<String> {
-        return userSettingLocalDataSource
-            .getLanguagePreference()
-            .flowOn(ioDispatcher)
+        return userSettingLocalDataSource.getLanguagePreference()
     }
 
     override suspend fun setThemePreference(theme: Theme) {
-        withContext(ioDispatcher) {
-            userSettingLocalDataSource.setThemePreference(theme.themeValue)
-        }
+        userSettingLocalDataSource.setThemePreference(theme.themeValue)
     }
 
     override fun getThemePreference(): Flow<Theme> {
         return userSettingLocalDataSource
             .getThemePreference()
             .map { Theme.fromValue(it) }
-            .flowOn(ioDispatcher)
     }
 }
