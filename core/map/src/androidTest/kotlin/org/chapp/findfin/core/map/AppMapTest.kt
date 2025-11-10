8e93da695ecf0231ba8cfc1330e5c242db7d3b17
chapp <chapp.hkg@gmail.com>
2025-11-10 23:46:32 +0800

    test(core-map): try fix the test

--- core/map/src/androidTest/kotlin/org/chapp/findfin/core/map/AppMapTest.kt
+++ core/map/src/androidTest/kotlin/org/chapp/findfin/core/map/AppMapTest.kt
@@ -5,18 +5,34 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.test.platform.app.InstrumentationRegistry
+import com.google.android.gms.maps.MapsInitializer
 import io.mockk.mockk
 import org.chapp.findfin.core.design.ui.foundation.modifier.contentDescription
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.util.concurrent.CountDownLatch
 
 class AppMapTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
+    @Before
+    fun setUp() {
+        val latch = CountDownLatch(1)
+        MapsInitializer.initialize(
+            InstrumentationRegistry.getInstrumentation().targetContext,
+            MapsInitializer.Renderer.LATEST,
+        ) {
+            latch.countDown()
+        }
+        latch.await()
+    }
+
     @Test
     fun testAppMap_init() {
-        val mockOnBoundsChange = mockk<((PositionBounds?) -> Unit)>()
+        val mockOnBoundsChange = mockk<((PositionBounds?) -> Unit)>(relaxed = true)
 
         composeTestRule.setContent {
             AppMap<Any>(
